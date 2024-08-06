294d1dbe397a49bebfad3f0c9e35817ea7a59343
Juan Garcia <jjgu95@gmail.com>
2024-08-06 17:36:41 -0500

    refactor(get_all_views): does not need loadenv

--- bi_functions/db_utils/get_all_views.py
+++ bi_functions/db_utils/get_all_views.py
@@ -1,22 +1,24 @@
 import psycopg2
-from dotenv import load_dotenv
-import os
 
-# Load environment variables from .env file
-load_dotenv()
-
-# Database connection parameters from environment variables
-db_params = {
-    'dbname': os.getenv('DB_NAME'),
-    'user': os.getenv('DB_USER'),
-    'password': os.getenv('DB_PASSWORD'),
-    'host': os.getenv('DB_HOST'),
-    'port': os.getenv('DB_PORT')
-}
-
-def get_all_views():
+def get_all_views(
+            dbname = None ,
+            user = None ,
+            password = None ,
+            host = None ,
+            port = None ):
+    
     # Establish a database connection
     try:
+
+        db_params = {
+            'dbname': dbname,
+            'user': user,
+            'password': password,
+            'host': host,
+            'port': port
+        }
+
+
         conn = psycopg2.connect(**db_params)
         cursor = conn.cursor()
         
