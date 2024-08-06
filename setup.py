25c01fe0cc872801c69d2bdc392f8143322a4066
Juan Garcia <jjgu95@gmail.com>
2024-08-06 18:42:24 -0500

    refactor(setup.py) refator to be correctly installed in the future

--- setup.py
+++ setup.py
@@ -3,14 +3,19 @@ from setuptools import setup, find_packages
 setup(
     name='bi-functions',
     version='0.1',
-    packages=find_packages(),
+    packages=find_packages(),  # This should include all subpackages
     install_requires=[
-        'psycopg2',
+        'psycopg2-binary',
         'python-dotenv',
     ],
-    entry_points={
-        'console_scripts': [
-            'get_all_views=bi_functions.db_utils.get_all_views:get_all_views',
-        ],
-    },
+    include_package_data=True,
+    description='A library of functions for BI applications',
+    author='Your Name',
+    author_email='your.email@example.com',
+    url='https://github.com/jgarcia-neostella/BI-Functions',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
 )
